81eb4912e9653dea6124872ba2fe350ae52c21cc
nabilElhady <nabilelhady73@gmail.com>
2024-07-11 14:13:47 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
-const express = require("express"); // Express framework for handling HTTP requests
-const app = express(); // Create an Express application
+const express = require("express");
+const path = require("path");
+const app = express();
+
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
@@ -8,7 +10,6 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const cors = require("cors");
-const path = require("path");
 require("dotenv").config({ path: "./config.env" });
 const mongoose = require("mongoose");
 
@@ -17,8 +18,11 @@ const corsOptions = {
   origin: "https://ecommerce-frontend-pi-fawn.vercel.app", // Replace with your frontend URL
   optionsSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json()); // Middleware to parse JSON requests
+
+// Serve static files from the /dev-data/images/products directory
 const imagesPath = path.join(__dirname, "./dev-data/images/products");
 console.log("Serving static files from:", imagesPath);
 app.use("/images/products", express.static(imagesPath));
@@ -30,17 +34,22 @@ app.use("/api/v1/orders", orderRoutes);
 app.use("/api/v1/payments", paymentRoutes);
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/v1/carts", cartRoutes);
+
 app.get("/api", (req, res) => {
   res.send({ message: "Hello from the backend!", db: DB });
 });
+
 app.get("/", (req, res) => {
   res.send({ message: "Hello from the backend!1", DB: DB });
 });
+
 console.log(DB);
 mongoose.connect(DB, {}).then((con) => {
   console.log("connection to db is done");
 });
+
 app.listen(8000, () => {
   console.log(`app is running on port 8000`);
 });
+
 module.exports = app;
