03f9a93377d2793a208b8be1c7e7045045e21258
nabilElhady <nabilelhady73@gmail.com>
2024-07-13 12:23:09 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -55,48 +55,48 @@
 // module.exports = app;
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const path = require("path");
-const mongoose = require("mongoose");
-require("dotenv").config({ path: "./config.env" });
+// const cors = require("cors");
+// const path = require("path");
+// const mongoose = require("mongoose");
+// require("dotenv").config({ path: "./config.env" });
 
-// Routes
-const productRoutes = require("./routes/productRoutes");
-const userRoutes = require("./routes/userRoutes");
-const reviewRoutes = require("./routes/reviewRoutes");
-const cartRoutes = require("./routes/cartRoutes");
-const paymentRoutes = require("./routes/paymentRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
+// // Routes
+// const productRoutes = require("./routes/productRoutes");
+// const userRoutes = require("./routes/userRoutes");
+// const reviewRoutes = require("./routes/reviewRoutes");
+// const cartRoutes = require("./routes/cartRoutes");
+// const paymentRoutes = require("./routes/paymentRoutes");
+// const orderRoutes = require("./routes/orderRoutes");
+// const categoryRoutes = require("./routes/categoryRoutes");
 
-// MongoDB Connection
-const DB = process.env.DataBaseURL;
-mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// // MongoDB Connection
+// const DB = process.env.DataBaseURL;
+// mongoose
+//   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log(err));
 
-// Middleware
-const corsOptions = {
-  origin: "https://ecommerce-frontend-pi-fawn.vercel.app", // Replace with your frontend URL
-  optionsSuccessStatus: 200,
-};
+// // Middleware
+// const corsOptions = {
+//   origin: "https://ecommerce-frontend-pi-fawn.vercel.app", // Replace with your frontend URL
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
-app.use(express.json());
+// app.use(cors(corsOptions));
+// app.use(express.json());
 
-const imagesPath = path.join(__dirname, "./dev-data/images/products");
-console.log("Serving static files from:", imagesPath);
-app.use("/images/products", express.static(imagesPath));
+// const imagesPath = path.join(__dirname, "./dev-data/images/products");
+// console.log("Serving static files from:", imagesPath);
+// app.use("/images/products", express.static(imagesPath));
 
-// Routes Middleware
-app.use("/api/v1/products", productRoutes);
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/reviews", reviewRoutes);
-app.use("/api/v1/orders", orderRoutes);
-app.use("/api/v1/payments", paymentRoutes);
-app.use("/api/v1/categories", categoryRoutes);
-app.use("/api/v1/carts", cartRoutes);
+// // Routes Middleware
+// app.use("/api/v1/products", productRoutes);
+// app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/reviews", reviewRoutes);
+// app.use("/api/v1/orders", orderRoutes);
+// app.use("/api/v1/payments", paymentRoutes);
+// app.use("/api/v1/categories", categoryRoutes);
+// app.use("/api/v1/carts", cartRoutes);
 
 app.get("/api", (req, res) => {
   res.send({ message: "Hello from the backend!", db: DB });
@@ -106,8 +106,7 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello from the backend!1" });
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(8000, () => {
   console.log(`App is running on port ${PORT}`);
 });
 
