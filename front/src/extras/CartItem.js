e63517819825d7973b1a050881d5485c32d805af
nabilElhady <nabilelhady73@gmail.com>
2024-07-15 10:26:35 +0300

    before was better

--- front/src/extras/CartItem.js
+++ front/src/extras/CartItem.js
@@ -1,45 +1,44 @@
-import React from "react";
-
 const CartItem = ({
   item,
   handleIncreaseItem,
   handleDecreaseItem,
   handleRemoveItem,
 }) => {
+  console.log(item);
   return (
-    <div className="flex flex-col sm:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4 bg-gray-800 p-4 rounded-lg mb-4">
+    <div className="flex justify-between items-center bg-gray-800 p-4 rounded-lg space-x-4">
       <img
         src={`${item.product.coverImage}`}
-        alt={item.product.title}
-        className="w-24 h-24 object-cover rounded-lg"
+        alt={item.product.name}
+        className="w-16 h-16 object-cover rounded-lg"
       />
-      <div className="flex flex-col items-center lg:items-start lg:flex-grow">
-        <h3 className="text-lg font-bold text-white">{item.product.title}</h3>
-        <p className="text-gray-400">${item.product.price.toFixed(2)}</p>
+      <div className="flex-1">
+        <h3 className="text-lg text-white font-semibold">
+          {item.product.name}
+        </h3>
+        <p className="text-sm text-gray-400">${item.product.price}</p>
       </div>
       <div className="flex items-center space-x-2">
         <button
-          className="bg-gray-700 text-white px-2 py-1 rounded-l"
           onClick={() => handleDecreaseItem(item.product._id)}
+          className="bg-gray-700 text-white px-2 py-1 rounded-lg"
         >
           -
         </button>
-        <span className="px-4 py-1 bg-gray-700 text-white border-t border-b">
-          {item.quantity}
-        </span>
+        <span className="text-white">{item.quantity}</span>
         <button
-          className="bg-gray-700 text-white px-2 py-1 rounded-r"
           onClick={() => handleIncreaseItem(item.product._id)}
+          className="bg-gray-700 text-white px-2 py-1 rounded-lg"
         >
           +
         </button>
+        <button
+          onClick={() => handleRemoveItem(item.product._id)}
+          className="bg-red-500 text-white px-2 py-1 rounded-lg"
+        >
+          Remove
+        </button>
       </div>
-      <button
-        className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition-colors"
-        onClick={() => handleRemoveItem(item.product._id)}
-      >
-        Remove
-      </button>
     </div>
   );
 };
