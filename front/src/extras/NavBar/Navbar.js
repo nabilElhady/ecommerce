039817316641f974ea805a6f1e6476c50ff9cec3
Nabil Elhady <nabilelhady73@gmail.com>
2024-08-15 14:16:44 +0300

    3

--- front/src/extras/NavBar/Navbar.js
+++ front/src/extras/NavBar/Navbar.js
@@ -254,12 +254,12 @@ const Navbar = () => {
           handleResultClick={handleResultClick}
         />
 
-        {showNotification && (
-          <div className="fixed top-0 left-1/2 transform -translate-x-1/2 mt-4 z-50 bg-green-500 text-white py-2 px-4 rounded-lg flex items-center shadow-lg transition-opacity duration-300">
-            <CheckCircleIcon className="w-6 h-6 mr-2" />
-            <span>done!</span>
-          </div>
-        )}
+        {showNotification &&
+          // <div className="fixed top-0 left-1/2 transform -translate-x-1/2 mt-4 z-50 bg-green-500 text-white py-2 px-4 rounded-lg flex items-center shadow-lg transition-opacity duration-300">
+          //   <CheckCircleIcon className="w-6 h-6 mr-2" />
+          //   <span>done!</span>
+          // </div>
+          alert("done")}
 
         <div className="flex items-center">
           <button
